0907eeb523445a2420a97ae1341dc345ca862ffa
DonCuervoC <nelson.cuervo89@gmail.com>
2022-12-04 14:25:40 -0500

    interface loggin et classe User, première version

---
+++ ChantierApp/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChantierApp
+{
+    internal class User
+    {
+
+        string? nom;
+        string? password;
+
+        public User(string? nom, string? password)
+        {
+            this.nom = nom;
+            this.password = password;
+        }
+    }
+}
