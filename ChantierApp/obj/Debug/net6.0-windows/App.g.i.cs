50e59313e7a0af0df929262d6c08587c061f18f6
Leonel Serrano <serranolsm@hotmail.com>
2022-12-29 22:53:05 -0500

    projet chantier correction click bouton

--- ChantierApp/obj/Debug/net6.0-windows/App.g.i.cs
+++ ChantierApp/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ChantierApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ChantierApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             ChantierApp.App app = new ChantierApp.App();
             app.InitializeComponent();
