d2c2d0f5920b23517961c6ad9217b84ffde627cc
DonCuervoC <nelson.cuervo89@gmail.com>
2023-01-09 16:26:23 -0500

    WPF App Chantier added

---
+++ AppChantier1.2/WpfAppChantier1.2/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppChantier1._2
+{
+
+    // cette classe est uniquement destinée à des fins de test. 
+    public class User
+    {
+        public string nom = "";
+        public string password = "";
+
+        public User(string nom, string password)
+        {
+            this.nom = nom;
+            this.password = password;
+        }
+    }
+}
