9a4440f8a2e3f8d6753db8759d99f4627875369e
DonCuervoC <nelson.cuervo89@gmail.com>
2023-01-30 20:40:08 -0500

    Update 1/30/2023 - interface ChefEquipeInter 95%, check how it refresh info in the list view ouvrages

--- WpfChantierApp1.2/ChefEquipeInter.xaml.cs
+++ WpfChantierApp1.2/ChefEquipeInter.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,8 +26,10 @@ namespace WpfChantierApp1._2
         Employe employeSession;
         int sessionEmployeEquipeID = 0;
         Equipe equipeSession;
-        Ouvrage ouvrageSession;
+        //Ouvrage ouvrageSession;
         int equipeID = 0;
+        //List<Ouvrage> ouvrlist = new List<Ouvrage>();
+        ObservableCollection<Ouvrage> ouvrlist = new ObservableCollection<Ouvrage>();
 
         public ChefEquipeInter(Employe employeSession)
         {
@@ -40,45 +44,47 @@ namespace WpfChantierApp1._2
                 // instance of Equipe
                 this.equipeSession = dbEntities.Equipes.SingleOrDefault(eq => eq.EquipeID == sessionEmployeEquipeID);
                 //instance of Ouvrage
-                this.ouvrageSession = dbEntities.Ouvrages.SingleOrDefault(ouvr => ouvr.OuvrageID == sessionEmployeEquipeID);
+                //this.ouvrageSession = dbEntities.Ouvrages.SingleOrDefault(ouvr => ouvr.OuvrageID == sessionEmployeEquipeID);
                 // set value with equipe ID session 
                 equipeID = equipeSession.EquipeID;
 
-                txtBlockNomEquipe.Text = "Equipe : " + equipeSession.NomDepartement;
+                txtBlockNomEquipe.Text = "Equipe : " + this.equipeSession.NomDepartement;
+                //txtBoxEquipeID.Text = this.employeSession.EquipeID.ToString();
 
-                afficherOuvrage();
+                AfficherOuvrage();
             }
 
         }
 
         private void btnAfficher_Click(object sender, RoutedEventArgs e)
         {
-            afficherOuvrage();
+            AfficherOuvrage();
         }
 
-        private void afficherOuvrage()
+        private void AfficherOuvrage()
         {
+            Ouvrage monOuvrage = null;
+
             using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
             {
-                var query = from ouvr in dbEntities.Ouvrages
-                            where ouvr.EquipeID == this.equipeID
-                            select new
-                            {
-                                OuvrageID = ouvr.OuvrageID,
-                                NomOuvrage = ouvr.NomOuvrage,
-                                Description_Ouvrage = ouvr.Description_Ouvrage,
-                                EquipeID = ouvr.EquipeID,
-                                Date_Debut_Ouvrage = ouvr.Date_Debut_Ouvrage,
-                                Date_Fin_Ouvrage = ouvr.Date_Fin_Ouvrage
-                            };
+                foreach (Ouvrage o in dbEntities.Ouvrages)
+                {
+                    if (o.EquipeID == this.equipeID)
+                    {
+                        monOuvrage = o;
+                        ouvrlist.Add(monOuvrage);
+                    }
+                }
 
-                ListViewOuvrage.ItemsSource = query.ToList();
-
+                if (ouvrlist.Count > 0) { ListViewOuvrage.ItemsSource = ouvrlist; }
+                else { MessageBox.Show("Actuellement, votre équipe n'a aucun ouvrage en cours"); }
             }
         }
 
+
         private void ListViewOuvrage_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Ouvrage ouvrageSelected = (Ouvrage)ListViewOuvrage.SelectedItem;
             int selectedEquipeID = 0;
 
             if (ListViewOuvrage.SelectedItem is Ouvrage ouvrage)
@@ -87,33 +93,166 @@ namespace WpfChantierApp1._2
                 txtBoxOuvrageID.Text = ouvrage.OuvrageID.ToString();
                 txtBoxNomOuvrage.Text = ouvrage.NomOuvrage;
                 txtBoxDescOuvrage.Text = ouvrage.Description_Ouvrage;
+                txtBoxEquipeID.Text = ouvrage.EquipeID.ToString();
                 //récupère l'Id de l'équipe pour chaque sélection 
                 selectedEquipeID = ouvrage.EquipeID.Value;
+            }
+            AfficherMateriaux(ouvrageSelected);
+            AfficherEmployes(selectedEquipeID);
+        }
+
+        private void AfficherEmployes(int selectedEquipeID)
+        {
+            using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
+            {
+                var query = from empl in dbEntities.Employes
+                            where empl.EmployeID == selectedEquipeID
+                            select new
+                            {
+                                EmployeID = empl.EmployeID,
+                                Nom = empl.Nom,
+                                Prenom = empl.Prenom,
+                                DateEmbauche = empl.DateEmbauche,
+                                Telephone = empl.Telephone,
+                                EquipeID = empl.EquipeID,
+                                PosteEmploi = empl.PosteEmploi,
+                                EmployeMotPasse = empl.EmployeMotPasse,
+                                // ValiderSanteSecuritaire = empl.ValiderSanteSecuritaire,
+                                Equipe = empl.Equipe
+                            };
+                ListViewTravailleurs.ItemsSource = query.ToList();
+            }
+        }
+
+        private void AfficherMateriaux(Ouvrage ouvrageSelected)
+        {
+            int ouvrageID = ouvrageSelected.OuvrageID;
+
+            using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
+            {
+                // comboBoxOuvrageID.ItemsSource = dbEntities.Ouvrages.ToList();
+                var query = from materiau in dbEntities.Materiauxes
+                            where materiau.OuvrageID == ouvrageID
+                            select new
+                            {
+                                MateriauxID = materiau.OuvrageID,
+                                NomMateriaux = materiau.NomMateriaux,
+                                DateReception = materiau.DateReception,
+                                OuvrageID = materiau.OuvrageID,
+                            };
+                ListViewMateriaux.ItemsSource = query.ToList();
+            }
+        }
+
+        // Rechercher la création et l'ajout d'un nouvel enregistrement dans la BD
+        private void btnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            string equipeIdCombo = txtBoxEquipeID.Text;
+            int equipeSelectedId = int.Parse(equipeIdCombo);
+
+            using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
+            {
+                // recherche le dernier élément stocké dans la table d'Ouvrages 
+                Ouvrage lastOuvrage = dbEntities.Ouvrages.ToArray().LastOrDefault();
+                // sauvegarde le dernier ID enregistré dans la table + 1 
+                int lastOuvrageID = lastOuvrage.OuvrageID + 1;
+
+                Equipe equipeCherche = dbEntities.Equipes.SingleOrDefault(x => x.EquipeID == equipeSelectedId); // requête LINQ
+
+                Ouvrage newOuvrage = new Ouvrage()
+                {
+                    OuvrageID = lastOuvrageID,
+                    NomOuvrage = txtBoxNomOuvrage.Text,
+                    Description_Ouvrage = txtBoxDescOuvrage.Text,
+
+                    EquipeID = this.equipeID,
+                    Date_Debut_Ouvrage = datePkrDebutOuvrage.SelectedDate.Value.ToString(),
+                    Date_Fin_Ouvrage = datePkrFinOuvrage.SelectedDate.Value.ToString(),
+                    Equipe = equipeCherche,
+                };
+
+                if (newOuvrage != null)
+                {
+                    dbEntities.Ouvrages.Add(newOuvrage);
+
+                    int resultat = dbEntities.SaveChanges();
+                    if (resultat > 0)
+                    {
+                        this.AfficherOuvrage();
+                        string message = $"L'ouvrage {newOuvrage.NomOuvrage} a été enregistré dans le système";
+                        MessageBox.Show(message);
+                    }
+                }
+            }
+        }
+
+        // Il modifie  un objet de type Ouvrage , recherche dans la BD les correspondances d'identifiants et modifie les informations de l'enregistrement.
+        private void btnModifier_Click(object sender, RoutedEventArgs e)
+        {
+            Ouvrage ouvrageSelected = (Ouvrage)ListViewOuvrage.SelectedItem;
+
+            if (ouvrageSelected != null)
+            {
+                using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
+                {
+                    Ouvrage ouvrModifier = dbEntities.Ouvrages.SingleOrDefault(ouvr => ouvr.OuvrageID == ouvrageSelected.OuvrageID);
+
+                    if (ouvrModifier != null)
+                    {
+                        ouvrModifier.OuvrageID = int.Parse(txtBoxOuvrageID.Text);
+                        ouvrModifier.NomOuvrage = txtBoxNomOuvrage.Text;
+                        ouvrModifier.Description_Ouvrage = txtBoxDescOuvrage.Text;
+                        ouvrModifier.EquipeID = int.Parse(txtBoxEquipeID.Text);
+                        ouvrModifier.Date_Debut_Ouvrage = datePkrDebutOuvrage.SelectedDate.Value.ToString();
+                        ouvrModifier.Date_Fin_Ouvrage = datePkrFinOuvrage.SelectedDate.Value.ToString();
+
+                        int resultat = dbEntities.SaveChanges();
+                        if (resultat > 0)
+                        {
+                            this.AfficherOuvrage();
+                            string message = $"L'ouvrage {ouvrModifier.NomOuvrage} a ete modifie";
+                            MessageBox.Show(message);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Réinitialise les zones de texte de l'interface avec une chaîne vide.
+        private void btnEffacer_Click(object sender, RoutedEventArgs e)
+        {
+            txtBoxOuvrageID.Text = "";
+            txtBoxNomOuvrage.Text = "";
+            txtBoxDescOuvrage.Text = "";
+            datePkrDebutOuvrage.Text = "";
+            datePkrFinOuvrage.Text = "";
+            //comboBoxEquipeID.Text = "";
+        }
+
+        // Crée un objet de type Ouvrage selon la sélection de l'utilisateur, fait une recherche dans la BD et s'il trouve des correspondances d'ID, le supprime. 
+        private void btnSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            Ouvrage ouvrageSelected = (Ouvrage)ListViewOuvrage.SelectedItem;
+
+            if (ouvrageSelected != null)
+            {
+                using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
+                {
+                    Ouvrage ouvrDeleted = dbEntities.Ouvrages.SingleOrDefault(ouvr => ouvr.OuvrageID == ouvrageSelected.OuvrageID);
+
+                    if (ouvrDeleted != null)
+                    {
+                        dbEntities.Ouvrages.Remove(ouvrDeleted);
+                        int resultat = dbEntities.SaveChanges();
+                        if (resultat > 0)
+                        {
+                            this.AfficherOuvrage();
+                            string message = $"L'ouvrage {ouvrDeleted.NomOuvrage} a ete supprime";
+                            MessageBox.Show(message);
+                        }
+                    }
+                }
             }
         }
     }
 }
-
-
-
-
-
-//private void AfficherMateriaux(Ouvrage ouvrageSelected)
-//{
-//    int ouvrageID = ouvrageSelected.OuvrageID;
-
-//    using (ProjetChantierEntities dbEntities = new ProjetChantierEntities())
-//    {
-//        comboBoxOuvrageID.ItemsSource = dbEntities.Ouvrages.ToList();
-//        var query = from materiau in dbEntities.Materiauxes
-//                    where materiau.OuvrageID == ouvrageID
-//                    select new
-//                    {
-//                        MateriauxID = materiau.OuvrageID,
-//                        NomMateriaux = materiau.NomMateriaux,
-//                        DateReception = materiau.DateReception,
-//                        OuvrageID = materiau.OuvrageID,
-//                    };
-//        ListViewMateriaux.ItemsSource = query.ToList();
-//    }
-//}
